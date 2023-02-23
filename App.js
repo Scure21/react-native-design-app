01c724aee6c6d80d4bb2dc99cd1092b4502f6a47
Stephanie Cure Chegwin <stephanie.cure@willowtreeapps.com>
2023-02-23 13:20:18 -0500

    Set up Apollo client

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
+import { APOLLO_CLIENT_TOKEN, SPACE_ID } from "@env";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 import { AppProvider } from "./context/appContext";
 import AppNavigator from "./navigator/AppNavigator";
 
+const client = new ApolloClient({
+  uri: `https://graphql.contentful.com/content/v1/spaces/${SPACE_ID}`,
+  credentials: "same-origin",
+  headers: {
+    Authorization: `Bearer ${APOLLO_CLIENT_TOKEN}`,
+  },
+});
+
 const App = () => (
-  <AppProvider>
-    <AppNavigator />
-  </AppProvider>
+  <ApolloProvider client={client}>
+    <AppProvider>
+      <AppNavigator />
+    </AppProvider>
+  </ApolloProvider>
 );
 
 export default App;
