48efb79d45e13ebff2af4a28dcac2b8edaca4453
Stephanie Cure Chegwin <stephanie.cure@willowtreeapps.com>
2023-02-27 13:28:21 -0500

    Create Project card and add pan gesture handler and animations

--- screens/ProjectsScreen.js
+++ screens/ProjectsScreen.js
@@ -1,10 +1,38 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useRef } from "react";
+import { Animated, PanResponder, StyleSheet, View } from "react-native";
+import Project from "../components/Project";
 
 const ProjectsScreen = () => {
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }], {
+        useNativeDriver: false,
+      }),
+      onPanResponderRelease: () => {
+        Animated.spring(pan, {
+          toValue: { x: 0, y: 0 },
+          useNativeDriver: false,
+        }).start();
+      },
+    })
+  ).current;
+
   return (
     <View style={styles.container}>
-      <Text>ProjectsScreen</Text>
+      <Animated.View
+        style={{ transform: [{ translateX: pan.x }, { translateY: pan.y }] }}
+        {...panResponder.panHandlers}
+      >
+        <Project
+          title="Price Tag"
+          imgSource={require("../assets/background5.jpg")}
+          author="Stephanie Cure"
+          text="Thanks to Design+Code, I improved my design skill and learned to do animations for my app Price Tag, a top news app in China."
+        />
+      </Animated.View>
     </View>
   );
 };
