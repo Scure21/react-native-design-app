bc3070afc70d8fd03b72cd481ffe40f5d22f665a
Stephanie Cure Chegwin <stephanie.cure@willowtreeapps.com>
2023-02-27 14:37:17 -0500

    Add gesture handler and animations for second card in the project cards stack
    - Set useNativeDriver to true so most animations run in the main thread (UI thread)
    - Allow for dropping card if Y position is greater than 400, else spring back to initial position

--- screens/ProjectsScreen.js
+++ screens/ProjectsScreen.js
@@ -1,21 +1,53 @@
 import React, { useRef } from "react";
 import { Animated, PanResponder, StyleSheet, View } from "react-native";
 import Project from "../components/Project";
+import { projectsData } from "../data/Projects";
 
 const ProjectsScreen = () => {
   const pan = useRef(new Animated.ValueXY()).current;
+  const scale = useRef(new Animated.Value(0.9)).current;
+  const translateY = useRef(new Animated.Value(44)).current;
 
   const panResponder = useRef(
     PanResponder.create({
+      onPanResponderGrant: () => {
+        Animated.spring(scale, { toValue: 1, useNativeDriver: true }).start();
+        Animated.spring(translateY, {
+          toValue: 0,
+          useNativeDriver: true,
+        }).start();
+      },
       onMoveShouldSetPanResponder: () => true,
       onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }], {
-        useNativeDriver: false,
+        useNativeDriver: false, // Note: panResponderMove doesn't work currently with nativeDriver
       }),
       onPanResponderRelease: () => {
-        Animated.spring(pan, {
-          toValue: { x: 0, y: 0 },
-          useNativeDriver: false,
-        }).start();
+        // Detect x and y position to know if we ned to drop the cards
+        const positionY = pan.y.__getValue();
+
+        // If positionY is grater than 400, drop the card so it's removed from the stack
+        if (positionY > 400) {
+          Animated.timing(pan, {
+            toValue: { x: pan.x, y: 1000 },
+            useNativeDriver: true,
+          }).start();
+        } else {
+          // Set first card to its initial position
+          Animated.spring(pan, {
+            toValue: { x: 0, y: 0 },
+            useNativeDriver: true,
+          }).start();
+
+          // Set second card to its initial position so it goes back
+          Animated.spring(scale, {
+            toValue: 0.9,
+            useNativeDriver: true,
+          }).start();
+          Animated.spring(translateY, {
+            toValue: 44,
+            useNativeDriver: true,
+          }).start();
+        }
       },
     })
   ).current;
@@ -27,10 +59,31 @@ const ProjectsScreen = () => {
         {...panResponder.panHandlers}
       >
         <Project
-          title="Price Tag"
-          imgSource={require("../assets/background5.jpg")}
-          author="Stephanie Cure"
-          text="Thanks to Design+Code, I improved my design skill and learned to do animations for my app Price Tag, a top news app in China."
+          title={projectsData[0].title}
+          imgSource={projectsData[0].image}
+          author={projectsData[0].author}
+          text={projectsData[0].text}
+        />
+      </Animated.View>
+
+      <Animated.View
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          zIndex: -1,
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          transform: [{ scale }, { translateY }],
+        }}
+      >
+        <Project
+          title={projectsData[1].title}
+          imgSource={projectsData[1].image}
+          author={projectsData[1].author}
+          text={projectsData[1].text}
         />
       </Animated.View>
     </View>
